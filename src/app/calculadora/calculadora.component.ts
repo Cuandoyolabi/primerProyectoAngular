337e32cfea4abcbe06717607c2f8db047c89fb38
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2024-08-28 16:03:18 -0600

    "Guardando"

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -8,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './calculadora.component.css'
 })
 export class CalculadoraComponent {
+    currentInput: string = "";
+    result: string = "";
 
+    appendNumber(num: string){
+      this.currentInput += num;
+    }
+
+    clear(){
+      this.currentInput = "";
+      this.result = "";
+    }
+
+    calculate(){
+      try{
+        this.result = this.simpleEvaluate(this.currentInput).toString();
+      } catch (e) {
+        this.result = 'Error';
+      }
+    }
+
+    private simpleEvaluate(expression: string): number {
+      return new Function('return' + expression)();
+    }
 }
